43cdab5ec397086710d93fbbccd7380565152dc3
ryosuke.fukui <fukuiryo.0312@gmail.com>
2020-02-16 16:01:28 +0000

    ・statusカラムを追加 ・カラム追加に伴いModel,Controller,Viewを更新 ・ControllerのDRYとViewのパーシャルを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
+  
+  before_action :set_task, only:[:show, :edit, :update, :destroy]
     
   def index
     @task = Task.all
   end
   
   def show
-    @task = Task.find(params[:id])
   end
     
   def new
@@ -25,12 +26,9 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
     
   def update
-    @task = Task.find(params[:id])
-    
     if @task.update(task_params)
       flash[:success] = 'タスクが正常に更新されました'
       redirect_to @task
@@ -41,7 +39,6 @@ class TasksController < ApplicationController
   end
     
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = 'タスクをリストから削除しました。お疲れ様でした。'
@@ -51,8 +48,14 @@ class TasksController < ApplicationController
   
   private
   
-  def task_params
-    params.require(:task).permit(:content)
+  def set_task
+    @task = Task.find(params[:id])
   end
+  
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+  
+
     
 end
