6fdb8b78406dae9cc56de881556847aa031f59d8
ryosuke.fukui <fukuiryo.0312@gmail.com>
2020-03-01 14:43:44 +0000

    ユーザー登録機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
     resources :tasks
     root to: 'tasks#index'
+    
+    get 'signup', to: 'users#new'
+    post 'users', to: 'users#create'
+    
 end
